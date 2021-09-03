835071d32e31847ecf24208eaa4b7c5605e53d3b
Ritesh Kumar <ritesh.kumar@roofstock.com>
2021-09-03 20:07:18 +0530

    Testing

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+function Profile()
+{
+    this.Name = "";
+    this.Address = "";
+    this.Qualification = "";
+    this.MaritalStatus = "";
+    this.Agree = "";
+    this.File = "";
+
+    this.Submit = function () {
+        
+    }
+}
+
+
+function SubmitForm()
+{
+    // var/const/let
+    var txtName = document.getElementById("txtName");
+    var txtAddress = document.getElementById("txtAddress");
+    
+}
